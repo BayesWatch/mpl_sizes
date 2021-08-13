22c8dcf14ecd3f6bb89c9681e3c8a7a706be1a9f
Jack Turner <jackwilliamturner@icloud.com>
2021-08-13 09:49:49 +0100

    fix setuptools licence

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
